b2bc937c3f977a5a08f512ad73cdad171bff7140
archistico <info@archistico.com>
2018-05-15 09:23:07 +0200

    script start

--- dist/js.0ecfb011.js
+++ dist/js.0ecfb011.js
@@ -121,7 +121,7 @@ var titolo = document.querySelector("#titolo");
 titolo.innerHTML = _hello2.default + " " + nome;
 
 console.log(_hello2.default + " " + nome);
-},{"./hello":6}],9:[function(require,module,exports) {
+},{"./hello":6}],8:[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -150,7 +150,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '50029' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '51579' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -291,5 +291,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},[9,4], null)
+},{}]},{},[8,4], null)
 //# sourceMappingURL=/js.0ecfb011.map
